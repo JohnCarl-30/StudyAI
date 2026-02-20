17e2446721e956c9a0d6f7c31c39f0e40edcfa68
JohnCarl-30 <johncarlsantos30@gmail.com>
2026-02-20 17:49:26 +0800

    fix: use langchain.text_splitter instead of langchain_text_splitters

--- backend/app/utils/chunking.py
+++ backend/app/utils/chunking.py
@@ -1,5 +1,5 @@
 from typing import List, Optional, Tuple
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document as LangchainDocument
 
 
